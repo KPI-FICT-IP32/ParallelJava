adf7330ac7dc658a5a9295e1d94b7c6bb6c01449
Oleksandr Kovalchuk <anxolerd@outlook.com>
2017-02-28 23:45:03 +0200

    [anxolerd::Lab01] initial

---
+++ Kovalchuk/lab01/src/main/java/edu/kpi/pjava/lab01/Main.java
@@ -0,0 +1,10 @@
+package edu.kpi.pjava.lab01;
+
+/**
+ * Created by oleksandr on 2/28/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("It is alive1111");
+    }
+}
