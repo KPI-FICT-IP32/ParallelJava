03f95061e7437804735ff727387631b3969ff5ec
Oleksandr Kovalchuk <anxolerd@outlook.com>
2017-04-01 13:50:35 +0300

    [anxolerd] Add description and url to project.clj

--- Kovalchuk/lab02/project.clj
+++ Kovalchuk/lab02/project.clj
@@ -1,6 +1,6 @@
 (defproject lab02 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "University assignments for Parallel Multi-Threaded Programming in Java course"
+  :url "https://github.com/KPI-FICT-IP32/ParallelJava"
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
